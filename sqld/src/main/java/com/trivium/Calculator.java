1c18a3a5e32836b13c4a4d693f7f019f553b1e8e
Shubham <shubhammishra4623@gmail.com>
2025-04-07 15:17:38 +0530

    First Commit

---
+++ sqld/src/main/java/com/trivium/Calculator.java
@@ -0,0 +1,14 @@
+package com.trivium;
+
+ class Calculator {
+
+	 int add(int a, int b) {
+		return a + b;
+	}
+	
+	 int subtract(int a, int b) {
+		return a - b;
+	}
+
+
+}
---
+++ sqld/src/test/java/com/trivium/CalculatorTest.java
@@ -0,0 +1,24 @@
+package com.trivium;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+ class CalculatorTest {
+	
+//	@Test
+//	 void testAdd() {
+//		Calculator calculator = new Calculator();
+//		int result = calculator.add(2, 3);
+//		assertEquals(5, result);
+//	}
+
+//	@Test
+//	 void testSubtract() {
+//		Calculator calculator = new Calculator();
+//		int result = calculator.subtract(5, 3);
+//		assertEquals(2, result);
+//	}
+
+
+}
